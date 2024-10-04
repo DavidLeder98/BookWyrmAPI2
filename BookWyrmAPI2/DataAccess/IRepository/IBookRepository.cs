83721e3941aba6ed253f83a095c3d1aaf3ad2a8a
David Leder <davidleder98@gmail.com>
2024-10-04 21:25:51 +0200

    i book repo initial

---
+++ BookWyrmAPI2/DataAccess/IRepository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookWyrmAPI2.Models.BaseModels;
+using BookWyrmAPI2.Models.DTOs.BookDTOs;
+
+namespace BookWyrmAPI2.DataAccess.IRepository
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<BookListDto>> GetBookListAsync();
+        Task<IEnumerable<BookListDto>> GetBookListAlphabeticallyAsync();
+        Task<IEnumerable<BookCardDto>> GetBookCardsAsync();
+    }
+}
