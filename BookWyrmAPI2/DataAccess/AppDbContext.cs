076abf5f25edb0b4345d6ef55cda1efc0b9900a6
David Leder <davidleder98@gmail.com>
2024-10-05 17:01:02 +0200

    seeded more data

--- BookWyrmAPI2/DataAccess/AppDbContext.cs
+++ BookWyrmAPI2/DataAccess/AppDbContext.cs
@@ -12,10 +12,28 @@ namespace BookWyrmAPI2.DataAccess
 
         }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // author to book
+            modelBuilder.Entity<Author>().HasMany(a => a.Books).WithOne(b => b.Author).HasForeignKey(a => a.AuthorId);
+
+            // book and category
+            modelBuilder.Entity<BookCategory>().HasKey(bc => new { bc.BookId, bc.CategoryId });
+            modelBuilder.Entity<BookCategory>().HasOne(bc => bc.Book).WithMany(b => b.BookCategories).HasForeignKey(bc => bc.BookId);
+            modelBuilder.Entity<BookCategory>().HasOne(bc => bc.Category).WithMany(c => c.BookCategories).HasForeignKey(bc => bc.CategoryId);
+
+            // seed authors
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, Name = "Gandalor Toadbottom", Description = "Alchemist, wizard, frog. He does it all!" },
+                new Author { Id = 2, Name = "Argus The Armless", Description = "One of the most respected experts in the field of dragon taming and dragon hunting." },
+                new Author { Id = 3, Name = "Mariana Trench", Description = "Where did she come from? What is she hiding?" },
+                new Author { Id = 4, Name = "Saif T. Ferst", Description = "The most well known author and professor from the Abjuration school of magic!" });
 
             // seed books
             modelBuilder.Entity<Book>().HasData(
@@ -23,13 +41,87 @@ namespace BookWyrmAPI2.DataAccess
                 {
                     Id = 1,
                     Title = "Brewing for Beginners",
-                    Description = "test 111",
+                    Description = "The best textbook on the market for aspiring alchemists!",
                     Rating = 4.1,
                     BestSeller = true,
-                    ImageUrl = "/uploads/books/Abj.png",
+                    ImageUrl = "Brew.png",
                     ListPrice = 29.99M,
                     Price = 24.99M,
+                    AuthorId = 1
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "Dragon Taming Safety Guide",
+                    Description = "Master Arguses magnum opus based on 30 years of dragon handling experience!",
+                    Rating = 4.8,
+                    BestSeller = true,
+                    ImageUrl = "Dragon.png",
+                    ListPrice = 39.99M,
+                    Price = 35.99M,
+                    AuthorId = 2
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "Secrets of the Deep",
+                    Description = "Ever wondered what lurks in the darkest, deepest corners of unexplored waters?",
+                    Rating = 4.4,
+                    BestSeller = false,
+                    ImageUrl = "Deep.png",
+                    ListPrice = 29.99M,
+                    Price = 27.99M,
+                    AuthorId = 3
+                },
+                new Book
+                {
+                    Id = 4,
+                    Title = "Abjuration Spells and Such",
+                    Description = "So you've chosen the most boring school of magic and you're worried you won't pass your exams because the dry study material? Worry now longer, with this book!",
+                    Rating = 4.4,
+                    BestSeller = false,
+                    ImageUrl = "Abj.png",
+                    ListPrice = 26.99M,
+                    Price = 25.99M,
+                    AuthorId = 4
+                },
+                new Book
+                {
+                    Id = 5,
+                    Title = "Alchemy of Gold",
+                    Description = "You're broke and down on your luck? Why not study alchemy? Maybe it's a better career path than gambling!",
+                    Rating = 2.4,
+                    BestSeller = false,
+                    ImageUrl = "Gold.png",
+                    ListPrice = 72.99M,
+                    Price = 69.99M,
+                    AuthorId = 1
                 });
+
+            // seed categories
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Spellcasting" },
+                new Category { Id = 2, Name = "Traveling" },
+                new Category { Id = 3, Name = "Combat" },
+                new Category { Id = 4, Name = "Alchemy" },
+                new Category { Id = 5, Name = "Bestiary" });
+
+            // seed bookcategories
+            modelBuilder.Entity<BookCategory>().HasData(
+                new BookCategory { BookId = 1, CategoryId = 4 },
+
+                new BookCategory { BookId = 2, CategoryId = 3 },
+                new BookCategory { BookId = 2, CategoryId = 5 },
+
+                new BookCategory { BookId = 3, CategoryId = 1 },
+                new BookCategory { BookId = 3, CategoryId = 2 },
+                new BookCategory { BookId = 3, CategoryId = 5 },
+
+                new BookCategory { BookId = 4, CategoryId = 1 },
+                new BookCategory { BookId = 4, CategoryId = 3 },
+
+                new BookCategory { BookId = 5, CategoryId = 1 },
+                new BookCategory { BookId = 5, CategoryId = 4 });
         }
     }
 }
---
+++ BookWyrmAPI2/DataAccess/Migrations/20241005112616_AthorAndCategorySeed.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookWyrmAPI2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AthorAndCategorySeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Books",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookCategories",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookCategories", x => new { x.BookId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_BookCategories_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Alchemist, wizard, frog. He does it all!", "Gandalor Toadbottom" },
+                    { 2, "One of the most respected experts in the field of dragon taming and dragon hunting.", "Argus The Armless" },
+                    { 3, "Where did she come from? What is she hiding?", "Mariana Trench" },
+                    { 4, "The most well known author and professor from the Abjuration school of magic!", "Saif T. Ferst" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AuthorId", "Description", "ImageUrl" },
+                values: new object[] { 1, "The best textbook on the market for aspiring alchemists!", "Brew.png" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Spellcasting" },
+                    { 2, "Traveling" },
+                    { 3, "Combat" },
+                    { 4, "Alchemy" },
+                    { 5, "Bestiary" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookCategories",
+                columns: new[] { "BookId", "CategoryId" },
+                values: new object[] { 1, 4 });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "BestSeller", "Description", "ImageUrl", "ListPrice", "Price", "Rating", "Title" },
+                values: new object[,]
+                {
+                    { 2, 2, true, "Master Arguses magnum opus based on 30 years of dragon handling experience!", "Dragon.png", 39.99m, 35.99m, 4.7999999999999998, "Dragon Taming Safety Guide" },
+                    { 3, 3, false, "Ever wondered what lurks in the darkest, deepest corners of unexplored waters?", "Deep.png", 29.99m, 27.99m, 4.4000000000000004, "Secrets of the Deep" },
+                    { 4, 4, false, "So you've chosen the most boring school of magic and you're worried you won't pass your exams because the dry study material? Worry now longer, with this book!", "Abj.png", 26.99m, 25.99m, 4.4000000000000004, "Abjuration Spells and Such" },
+                    { 5, 1, false, "You're broke and down on your luck? Why not study alchemy? Maybe it's a better career path than gambling!", "Gold.png", 72.99m, 69.99m, 2.3999999999999999, "Alchemy of Gold" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookCategories",
+                columns: new[] { "BookId", "CategoryId" },
+                values: new object[,]
+                {
+                    { 2, 3 },
+                    { 2, 5 },
+                    { 3, 1 },
+                    { 3, 2 },
+                    { 3, 5 },
+                    { 4, 1 },
+                    { 4, 3 },
+                    { 5, 1 },
+                    { 5, 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategories_CategoryId",
+                table: "BookCategories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "BookCategories");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "ImageUrl" },
+                values: new object[] { "test 111", "/uploads/books/Abj.png" });
+        }
+    }
+}
