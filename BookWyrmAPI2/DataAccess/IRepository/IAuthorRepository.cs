3ef1697cf2a4495b226db5dd4c2d7ae30e447eca
David Leder <davidleder98@gmail.com>
2024-10-06 21:11:05 +0200

    author interface

---
+++ BookWyrmAPI2/DataAccess/IRepository/IAuthorRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookWyrmAPI2.DataAccess.Repository;
+using BookWyrmAPI2.Models.BaseModels;
+using BookWyrmAPI2.Models.DTOs.AuthorDTOs;
+using static BookWyrmAPI2.DataAccess.Repository.BookRepository;
+
+namespace BookWyrmAPI2.DataAccess.IRepository
+{
+    public interface IAuthorRepository
+    {
+        Task<IEnumerable<AuthorListDto>> GetAuthorsListAsync(string? searchTerm);
+        Task<AuthorWithBooksDto> GetAuthorByIdAsync(int id, SortBy sortBy);
+        Task<Author> CreateAuthorAsync(AuthorCreateDto authorCreateDto);
+        Task<Author> UpdateAuthorAsync(AuthorUpdateDto authorUpdateDto);
+        Task<Author> DeleteAuthorAsync(int id);
+    }
+}
