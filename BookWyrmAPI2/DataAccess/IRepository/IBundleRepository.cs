2c8c0b4352a3f7ec13dfba409c7468b368b2918f
David Leder <davidleder98@gmail.com>
2024-10-06 21:11:21 +0200

    bundle interface

---
+++ BookWyrmAPI2/DataAccess/IRepository/IBundleRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookWyrmAPI2.Models.BaseModels;
+using BookWyrmAPI2.Models.DTOs.AuthorDTOs;
+using BookWyrmAPI2.Models.DTOs.BundleDTOs;
+
+namespace BookWyrmAPI2.DataAccess.IRepository
+{
+    public interface IBundleRepository
+    {
+        Task<IEnumerable<BundleListDto>> GetBundlesListAsync();
+        Task<BundleWithBooksDto> GetBundleByIdAsync(int id); //for users to view books in bundle
+        Task<BundleWithBookListDto> GetBundleWithBookListAsync(int id); //for admins to view in the content managment panel
+        Task<Bundle> CreateBundleAsync(BundleCreateDto bundleCreateDto);
+        Task<Bundle> UpdateBundleAsync(BundleUpdateDto bundleUpdateDto);
+        Task<Bundle> DeleteBundleAsync(int id);
+    }
+}
