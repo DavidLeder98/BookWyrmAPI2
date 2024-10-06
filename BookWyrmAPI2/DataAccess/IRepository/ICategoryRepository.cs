04018fcd48df6d8da3f8f362ac966322a6a56f54
David Leder <davidleder98@gmail.com>
2024-10-06 21:11:28 +0200

    category interface

---
+++ BookWyrmAPI2/DataAccess/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookWyrmAPI2.Models.BaseModels;
+using BookWyrmAPI2.Models.DTOs.BookDTOs;
+using BookWyrmAPI2.Models.DTOs.CategoryDTOs;
+using static BookWyrmAPI2.DataAccess.Repository.BookRepository;
+
+namespace BookWyrmAPI2.DataAccess.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<CategoryListDto>> GetCategoriesListAsync();
+        Task<CategoryWithBooksDto> GetCategoryByIdAsync(int id, SortBy sortBy);
+        Task<Category> CreateCategoryAsync(CategoryCreateDto categoryCreateDto);
+        Task<Category> UpdateCategoryAsync(CategoryUpdateDto categoryUpdateDto);
+        Task<Category> DeleteCategoryAsync(int id);
+    }
+}
