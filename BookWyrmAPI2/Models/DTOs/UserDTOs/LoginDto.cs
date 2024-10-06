442d1cead20c5417db0d5bb60a8e8dd8df203665
David Leder <davidleder98@gmail.com>
2024-10-06 21:09:09 +0200

    user dtos

---
+++ BookWyrmAPI2/Models/DTOs/UserDTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWyrmAPI2.Models.DTOs.UserDTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters long.")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long. It must contain an uppercase letter, a number and a non alphanumeric character.")]
+        public string Password { get; set; }
+    }
+}
---
+++ BookWyrmAPI2/Models/DTOs/UserDTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWyrmAPI2.Models.DTOs.UserDTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters long.")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long. It must contain an uppercase letter, a number and a non alphanumeric character.")]
+        public string Password { get; set; }
+    }
+}
