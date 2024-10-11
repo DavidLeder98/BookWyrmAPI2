c88438ced06fe2110a653f3f0e244cf3dc06fa78
David Leder <davidleder98@gmail.com>
2024-10-11 17:31:53 +0200

    seeded more data

---
+++ BookWyrmAPI2/DataAccess/Migrations/20241009144247_LargeImages.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookWyrmAPI2.Migrations
+{
+    /// <inheritdoc />
+    public partial class LargeImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LargeImageUrl",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LargeImageUrl",
+                value: "LargeBrew.png");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LargeImageUrl",
+                value: "LargeDragon.png");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "LargeImageUrl",
+                value: "LargeDeep.png");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "LargeImageUrl",
+                value: "LargeAbj.png");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "LargeImageUrl",
+                value: "LargeGold.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LargeImageUrl",
+                table: "Books");
+        }
+    }
+}
