b3234e010d1a9815acdbb09823fdc50a16bcd0b3
David Leder <davidleder98@gmail.com>
2024-11-05 17:23:54 +0100

    user dtos updated

--- BookWyrmAPI2/Models/DTOs/UserDTOs/RegisterDto.cs
+++ BookWyrmAPI2/Models/DTOs/UserDTOs/RegisterDto.cs
@@ -5,15 +5,17 @@ namespace BookWyrmAPI2.Models.DTOs.UserDTOs
     public class RegisterDto
     {
         [Required]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters long.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters long.")]
+        [RegularExpression(@"^[a-zA-Z0-9._]+$", ErrorMessage = "Username can only contain letters, numbers, '.' and '_'")]
         public string Username { get; set; }
 
         [Required]
-        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [EmailAddress(ErrorMessage = "Please provide a valid email address.")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long. It must contain an uppercase letter, a number and a non alphanumeric character.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$", ErrorMessage = "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.")]
         public string Password { get; set; }
     }
 }
---
+++ BookWyrmAPI2/Models/DTOs/UserDTOs/UserProfileDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BookWyrmAPI2.Models.DTOs.UserDTOs
+{
+    public class UserProfileDto
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? TelNumber { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? ZipCode { get; set; }
+    }
+}
