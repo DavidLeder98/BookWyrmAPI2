41bfd2b5504de8f0961f1ded7f6a97f60c21b538
David Leder <davidleder98@gmail.com>
2024-11-05 17:24:29 +0100

    user validation service added

---
+++ BookWyrmAPI2/Services/CustomUserValidator.cs
@@ -0,0 +1,25 @@
+﻿using BookWyrmAPI2.Models.BaseModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookWyrmAPI2.Services
+{
+    public class CustomUserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            var errors = new List<IdentityError>();
+
+            // Check username length
+            if (user.UserName.Length > 20)
+            {
+                errors.Add(new IdentityError
+                {
+                    Code = "InvalidUserName",
+                    Description = "The username must be at most 20 characters long."
+                });
+            }
+
+            return Task.FromResult(errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+        }
+    }
+}
